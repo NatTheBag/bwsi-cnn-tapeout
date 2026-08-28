e225622362984fb32400c93229aff747cfdcdaa3
Toribyte-P <tori.porat@gmail.com>
2026-08-27 20:13:00 -0700

    Modify module instantiation in tb.v
    Updated module instantiation for user_project.

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_im2col_tm (
+  tt_um_im2col_tm user_project(
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
